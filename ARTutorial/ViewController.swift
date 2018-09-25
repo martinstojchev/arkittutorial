bc50a8840bd812ef1a9782a5326db7e9d24732e6
Martin <martinstojchev@hotmail.com>
2018-09-25 10:52:16 +0200

    Placing balls in world

--- ARTutorial/ViewController.swift
+++ ARTutorial/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
+    var planes = [ARPlaneAnchor: Plane]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -28,6 +31,10 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Set the scene to the view
         sceneView.scene = scene
+        
+        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+        
+      
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -35,6 +42,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
+        
+        configuration.planeDetection = .horizontal
 
         // Run the view's session
         sceneView.session.run(configuration)
@@ -45,6 +54,65 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Pause the view's session
         sceneView.session.pause()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else {return}
+        let result = sceneView.hitTest(touch.location(in: sceneView), types: [ARHitTestResult.ResultType.featurePoint])
+        guard let hitResult = result.last else {return}
+        let hitTransform = SCNMatrix4(hitResult.worldTransform)
+        let hitVector = SCNVector3Make(hitTransform.m41, hitTransform.m42, hitTransform.m43)
+        createBall(position: hitVector)
+        
+       
+        
+    }
+    
+    
+    
+    func createBall(position: SCNVector3) {
+        var ballShape = SCNSphere(radius: 0.01)
+        var ballNode = SCNNode(geometry: ballShape)
+        ballNode.position = position
+        sceneView.scene.rootNode.addChildNode(ballNode)
+        
+    }
+
+    
+
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        
+        DispatchQueue.main.async {
+            if let planeAnchor = anchor as? ARPlaneAnchor {
+                self.addPlane(node: node, anchor: planeAnchor)
+            }
+        }
+    }
+    func renderer(_ renderer: SCNSceneRenderer, didRemove node: SCNNode, for anchor: ARAnchor) {
+        
+    }
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        
+        DispatchQueue.main.async {
+            if let planeAnchor = anchor as? ARPlaneAnchor {
+                self.updatePlane(anchor: planeAnchor)
+                
+            }
+        }
+    }
+    
+    func addPlane(node: SCNNode, anchor: ARPlaneAnchor) {
+        
+        
+        let plane = Plane(anchor)
+        node.addChildNode(plane)
+        planes[anchor] = plane
+    }
+    
+    func updatePlane(anchor: ARPlaneAnchor){
+        if let plane = planes[anchor] {
+            plane.update(anchor)
+        }
     }
 
     // MARK: - ARSCNViewDelegate
@@ -57,6 +125,27 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         return node
     }
 */
+    
+    func createTextNode(string: String) -> SCNNode {
+        let text = SCNText(string: string, extrusionDepth: 0.1)
+        text.font = UIFont.systemFont(ofSize: 1.0)
+        text.flatness = 0.01
+        text.firstMaterial?.diffuse.contents = UIColor.white
+        
+        let textNode = SCNNode(geometry: text)
+        
+        let fontSize = Float(0.04)
+        textNode.scale = SCNVector3(fontSize, fontSize, fontSize)
+        
+        return textNode
+    }
+    
+    func addText(string: String, parent: SCNNode) {
+        let textNode = self.createTextNode(string: string)
+        textNode.position = SCNVector3Zero
+        
+        parent.addChildNode(textNode)
+    }
     
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
